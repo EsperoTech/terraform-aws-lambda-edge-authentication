a0e3c1975bffd52328ab3e6433f99d4b5152efe7
Julian Fh <julian-fh@noreply.users.github.com>
2021-03-01 12:08:41 +0100

    - added configurable version of static auth script

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import {BasicAuth} from './basic_auth';
-
-exports.handler = async (event, context, callback) => {
-    const auth = new BasicAuth();
-    return auth.handler(event, context, callback);
-};
