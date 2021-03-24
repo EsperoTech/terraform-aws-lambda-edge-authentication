3e954a655d8fab3172493d62de3532aee4e4234d
julian-fh <julian-fh@users.noreply.github.com>
2021-03-24 14:44:22 +0100

    Add unprotected paths
    We need this to exclude e.g. resources from password protection

--- src/main.js
+++ src/main.js
@@ -22,10 +22,11 @@ exports.handler = (event, context, callback) => {
     const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
 
     const authStrings = [ authString ];
-    const protectedPaths = [ "^\/.+$" ];
+    const protectedPaths = [ "^\/.+$"];
+    const unprotectedPaths = ["^\/res\/.+$"];
 
     // Require Basic authentication
-    if (isProtectedURL(request.uri, protectedPaths) && 
+    if (isProtectedURL(request.uri, protectedPaths, unprotectedPaths) && 
         (typeof headers.authorization == 'undefined' || !authStrings.includes(headers.authorization[0].value))) {
 
         const body = 'Unauthorized';
@@ -44,10 +45,17 @@ exports.handler = (event, context, callback) => {
     callback(null, request);
 };
 
-function isProtectedURL(path, protectedPaths) {
+function isProtectedURL(path, protectedPaths, unprotectedPaths) {
     let i = 0;
+    let ii = 0;
     for (; i < protectedPaths.length; i++) {
         if (path.match(protectedPaths[i])) {
+            for(; ii < unprotectedPaths.length; ii++) {
+                if (path.match(unprotectedPaths[ii])) {
+                    return false;
+                }
+            }
+            
             return true;
         }
     }
